2df5c42dbf190cc29b33be5804950f59a5947a8b
daltonpurnell <daltonpurnell@live.com>
2015-04-17 23:18:57 -0600

    project complete

--- CanRemind/CRViewController.m
+++ CanRemind/CRViewController.m
@@ -9,10 +9,13 @@
 #import "CRViewController.h"
 
 @interface CRViewController ()
+@property (weak, nonatomic) IBOutlet UIDatePicker *datePicker;
 
 @end
 
 @implementation CRViewController
+
+
 
 - (void)viewDidLoad
 {
@@ -20,10 +23,56 @@
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
+
+- (IBAction)remindIn10Seconds {
+    NSDate *alertTime = [[NSDate date] dateByAddingTimeInterval:10];
+    
+    UILocalNotification *localNotification = [UILocalNotification new];
+    
+    if (localNotification) {
+        localNotification.fireDate = alertTime;
+        localNotification.timeZone = [NSTimeZone defaultTimeZone];
+        localNotification.repeatInterval = 0;
+        localNotification.soundName = @"bell_tree.mp3";
+        localNotification.alertBody = @"This is a 10 second notification";
+        localNotification.applicationIconBadgeNumber = 1;
+        
+        [[UIApplication sharedApplication] scheduleLocalNotification:localNotification];
+        
+    }
+    
+}
+
+
+- (IBAction)remindOnDate {
+    
+    NSDate *alertTime = self.datePicker.date;
+    UILocalNotification *localNotification = [UILocalNotification new];
+    
+    if (localNotification) {
+        localNotification.fireDate = alertTime;
+        localNotification.timeZone = [NSTimeZone defaultTimeZone];
+        localNotification.repeatInterval = 0;
+        localNotification.soundName = @"bell_tree.mp3";
+        localNotification.alertBody = @"This is a date picker notification";
+        localNotification.applicationIconBadgeNumber = 1;
+        
+        [[UIApplication sharedApplication] scheduleLocalNotification:localNotification];
+        
+    }
+}
+
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+
+
+
+
+
+
 
 @end
